70576d4123d93c3d840a2c1c6215f3d08bb7c858
Johnson <johnson111788@gmail.com>
2019-05-13 23:07:08 +0800

    First Commit

---
+++ Plant/Plant/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Plant
+//
+//  Created by 周昱程 on 2019/5/13.
+//  Copyright © 2019 周昱程. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
